b71bd0a1b7e3d113e83a3c8c829c57de5ad19569
Aditi Soni <soniaditi4623@gmail.com>
2021-02-14 03:24:22 +0530

    changed port

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT=process.env.PORT || 3300
+const PORT=process.env.PORT || 8081
 const {ADD_MEME,GET_MEME,DELETE_MEME}=require('./query')
 
 app.post("/", ADD_MEME);
