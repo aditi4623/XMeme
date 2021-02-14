833bd31fdeeb6b2c1284dd3265363feaad0ee102
Aditi Kumari <soniaditi4623@gmail.com>
2021-02-14 17:58:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-sudo apt-get install nodejs -y
-cd Frontend 
-npm install
-npm start 
+sudo apt install -y nodejs
 
 # Any configuration related commands
 
